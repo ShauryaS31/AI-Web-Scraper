869258868991feccf5d9df3063199e0afb7200eb
Shaurya Sharma <shauryasharma3108@gmail.com>
2024-10-28 15:58:10 +1100

    Second commit

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import (
+    scrape_website,
+    extract_body_content,
+    clean_body_content,
+    split_dom_content,
+)
 
+# Streamlit UI
 st.title("AI Web Scraper")
-url = st.text_input("Enter the Website URL: ")
+url = st.text_input("Enter Website URL")
 
-if st.button("Scrape Data"):
-   st.write("Scraping data from the website...")
-   result = scrape_website(url)
-   print(result)
+# Step 1: Scrape the Website
+if st.button("Scrape Website"):
+    if url:
+        st.write("Scraping the website...")
 
+        # Scrape the website
+        dom_content = scrape_website(url)
+        body_content = extract_body_content(dom_content)
+        cleaned_content = clean_body_content(body_content)
+
+        # Store the DOM content in Streamlit session state
+        st.session_state.dom_content = cleaned_content
+
+        # Display the DOM content in an expandable text box
+        with st.expander("View DOM Content"):
+            st.text_area("DOM Content", cleaned_content, height=300)
--- scrape_BD.py
+++ scrape_BD.py
@@ -1,24 +1,33 @@
-import selenium.webdriver as webdriver
-from selenium.webdriver.chrome.service import Service
-import time
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SBR_WEBDRIVER = os.getenv("SBR_WEBDRIVER")
+
 
 def scrape_website(website):
-    print("Launching Chrome Browser.... ")
-
-    chrome_driver_path = "chromedriver.exe"
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-
-    try:
+    print("Connecting to Scraping Browser...")
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, "goog", "chrome")
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
         driver.get(website)
-        print("Website is loaded")
+        print("Waiting captcha to solve...")
+        solve_res = driver.execute(
+            "executeCdpCommand",
+            {
+                "cmd": "Captcha.waitForSolve",
+                "params": {"detectTimeout": 10000},
+            },
+        )
+        print("Captcha solve status:", solve_res["value"]["status"])
+        print("Navigated! Scraping page content...")
         html = driver.page_source
-        time.sleep(10)
-
         return html
-    finally:
-        driver.quit()
+
+
 def extract_body_content(html_content):
     soup = BeautifulSoup(html_content, "html.parser")
     body_content = soup.body
